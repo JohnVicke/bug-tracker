7711f4e0331557c603eae42e78de874eee49cadf
Viktor Malmedal <viktormalmedal@gmail.com>
2021-10-14 10:50:54 +0200

    feat: extend loginmodal to handle register

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,4 +1,4 @@
 import dotenv from "dotenv";
 dotenv.config();
 export const PORT = process.env.PORT || 42069;
-export const __prod__ = process.env.NODE_ENV !== "development";
+export const __prod__ = process.env.NODE_ENV === "prod";
